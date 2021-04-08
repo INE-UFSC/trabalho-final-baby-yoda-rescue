abc45bf187d9313e93ab9713284b6c065bad6c09
Your Name <you@example.com>
2021-04-08 19:54:30 -0300

    Removi código que não utilizaremos nessa versão

--- prototipo/enemy.py
+++ prototipo/enemy.py
@@ -9,11 +9,3 @@ class Enemy(pygame.sprite.Sprite):
         #self.rect = self.surf.get_rect(center = (random.randint(40,SCREEN_WIDTH-40), 0))
 
       def move(self):
-        '''
-        global SCORE
-        self.rect.move_ip(0,SPEED)
-        if (self.rect.bottom > 600):
-            SCORE += 1
-            self.rect.top = 0
-            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
-        '''
